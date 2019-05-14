d6a31cd193fea2cbb704692056a156a43d81362e
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-05-15 00:34:25 +0900

    postしてみる

--- app.rb
+++ app.rb
@@ -13,8 +13,6 @@ post '/' do
 
     params = JSON.parse request.body.read
 
-    p params['payload']['all_commit_details'][0]['commit_url']
-=begin
     payload = Payload.new(params['payload']['reponame'],
                           params['payload']['outcome'],
                           params['payload']['branch'],
@@ -24,5 +22,6 @@ post '/' do
                           params['payload']['build_url'],
                           params['payload']['build_num'],
                           ENV["WEBHOOKURL"])
-=end
+
+    payload.post
 end
