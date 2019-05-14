0ecd5ded6b78d8987079ad9167375409e21787c9
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-05-14 23:44:25 +0900

    不要な部分を削除

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require 'sinatra'
 require 'rack/contrib'
 require 'json'
-require 'net/http'
 require 'dotenv'
 require 'uri'
 require './payload.rb'
@@ -19,7 +18,7 @@ post '/' do
     #puts params
 
     uri = URI.parse(ENV["WEBHOOKURL"])
-=begin
+
     payload = Payload.new(params['payload']['reponame'],
                           params['payload']['outcome'],
                           params['payload']['branch'],
@@ -29,11 +28,8 @@ post '/' do
                           params['payload']['build_url'],
                           params['payload']['build_num'],
                           uri)
-=end
-    payload = Payload.new("a","b","c","d","e","f","g",uri)
+
     payload.log
 
-   
-   Net::HTTP.post_form(uri, {payload: payload.to_json})
 
 end
