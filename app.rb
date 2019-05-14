9a72f5c8c3b84384753f0d20c835684e41e060d1
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-05-14 23:33:45 +0900

    邪魔な部分を消す

--- app.rb
+++ app.rb
@@ -33,41 +33,7 @@ post '/' do
     payload = Payload.new("a","b","c","d","e","f","g",uri)
     payload.log
 
-   payload = {
-       attachments: [
-           {
-               
-               title: "#{params['payload']['reponame']} CircleCI結果",
-               thumb_url: thumb_url,
-               pretext: "<!channel> #{pretext}",
-               text: params['payload']['outcome'],
-               fields: [
-                {
-                    title: "branch",
-                    value: params['payload']['branch'],
-                    short: "true"
-               },
-                {
-                    title: "committer_name",
-                    value: params['payload']['committer_name'],
-                    short: "true"
-               },
-               {
-                    title: "commit_url",
-                    value: "<#{params['payload']['all_commit_details'][0]['commit_url']} | #{params['payload']['subject']} >",
-                    short: "true"
-               },
-               {
-                    title: "build_url",
-                    value: "<#{params['payload']['build_url']} | ##{params['payload']['build_num']} >",
-                    short: "true"
-                }
-               ],
-               color: color
-           }
-       ]
-   }
-
+   
    Net::HTTP.post_form(uri, {payload: payload.to_json})
 
 end
