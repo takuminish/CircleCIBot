ac6fee568df2516db7ada791dcfafcf6bb99d989
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-05-14 22:56:51 +0900

    initializeの完成

--- payload.rb
+++ payload.rb
@@ -1,13 +1,13 @@
 class Payload
-    def initialize()
-        @reponame
-        @pretext
-        @outcome
-        @branch
-        @commiter_name
-        @commit_url
-        @build_url
-        @color
+    def initialize(reponame, pretext, outcome, branch, commiter_name, commit_url, build_url, webhook_url)
+        @reponame = reponame
+        @pretext = pretext
+        @outcome = outcome
+        @branch = branch
+        @commiter_name = commiter_name
+        @commit_url = commit_url
+        @build_url = build_url
+        @webhook_url = webhook_url
 
     end
 
